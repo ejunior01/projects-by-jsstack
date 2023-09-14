41557e12207b4830f5f63b06624388d2660827c3
ejunior01 <jk.jr47@gmail.com>
2023-09-14 00:43:20 -0300

    Mock de dados

---
+++ first-api/src/mocks/users.js
@@ -0,0 +1,21 @@
+const users = [
+    {
+        id: 1,
+        name: "Mateus"
+    },
+    {
+        id: 2,
+        name: "Jaque"
+    },
+    {
+        id: 3,
+        name: "Junior"
+    },
+    {
+        id: 4,
+        name: "Fred"
+    },
+]
+
+
+module.exports = users
