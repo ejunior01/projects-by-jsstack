e90d8378f51928b8f422b80bbadd64f64ac989b8
ejunior01 <jk.jr47@gmail.com>
2023-09-14 01:39:32 -0300

    Implementado o método getUserById, e refatorado para aceitar params

--- first-api/src/controllers/UserController.js
+++ first-api/src/controllers/UserController.js
@@ -5,7 +5,7 @@ module.exports = {
         const { order } = request.query
 
         const sordtedUser = users.sort((a, b) => {
-            if (order.toLocaleLowerCase() === "desc") {
+            if (order === "desc") {
                 return a.id < b.id ? 1 : -1
             }
             return a.id > b.id ? 1 : -1
@@ -14,5 +14,21 @@ module.exports = {
 
         response.writeHead(200, { "Content-Type": "application\json" })
         response.end(JSON.stringify(sordtedUser))
+    },
+    getUserById(request, response) {
+
+        const { id } = request.params
+
+        const user = users.find((userObj) => userObj.id === Number(id))
+
+        if (user) {
+            response.writeHead(200, { "Content-Type": "application\json" })
+            response.end(JSON.stringify(user))
+        } else {
+            response.writeHead(400, { "Content-Type": "application\json" })
+            response.end(JSON.stringify({ error: "User not found" }))
+        }
+
+
     }
 }
--- first-api/src/index.js
+++ first-api/src/index.js
@@ -5,12 +5,23 @@ const { URL } = require("node:url")
 const server = http.createServer((request, response) => {
     console.log(`Request method ${request.method} | Endpoint ${request.url}`)
 
-    const parserdUrl = new URL(`http://localhost:3000${request.url}`) 
-    
-    const route = routes.find((routeObj) => routeObj.endpoint == parserdUrl.pathname && routeObj.method == request.method)
+    const parserdUrl = new URL(`http://localhost:3000${request.url}`)
+
+    let { pathname } = parserdUrl
+    let id = null
+
+    const splitEndpoint = pathname.split("/").filter(Boolean)
+
+    if (splitEndpoint.length > 1) {
+        pathname = `/${splitEndpoint[0]}/:id`
+        id = splitEndpoint[1]
+    }
+
+    const route = routes.find((routeObj) => routeObj.endpoint == pathname && routeObj.method == request.method)
 
     if (route) {
         request.query = Object.fromEntries(parserdUrl.searchParams)
+        request.params = { id }
         route.handler(request, response)
     } else {
         response.writeHead(404, { "Content-Type": "text\html" })
