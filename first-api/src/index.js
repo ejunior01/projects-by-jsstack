d88ff01874e89f3267ab40605c38b19e526a7c00
ejunior01 <jk.jr47@gmail.com>
2023-09-14 01:15:45 -0300

    Implementado a ordenação no listusers

--- first-api/src/index.js
+++ first-api/src/index.js
@@ -1,17 +1,20 @@
 const http = require("http")
 const routes = require("./routes")
-
+const { URL } = require("node:url")
 
 const server = http.createServer((request, response) => {
     console.log(`Request method ${request.method} | Endpoint ${request.url}`)
 
-    const route = routes.find((routeObj) => routeObj.endpoint == request.url && routeObj.method == request.method)
+    const parserdUrl = new URL(`http://localhost:3000${request.url}`) 
+    
+    const route = routes.find((routeObj) => routeObj.endpoint == parserdUrl.pathname && routeObj.method == request.method)
 
     if (route) {
+        request.query = Object.fromEntries(parserdUrl.searchParams)
         route.handler(request, response)
     } else {
         response.writeHead(404, { "Content-Type": "text\html" })
-        response.end(`Cannot ${request.method} ${request.url}`)
+        response.end(`Cannot ${request.method} ${parserdUrl.pathname}`)
     }
 
 })
