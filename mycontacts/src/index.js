f50949d21ee0c8dad036adc2d303249b8cd4dbad
ejunior01 <jk.jr47@gmail.com>
2023-09-19 23:18:53 -0300

    Adição da routa delete, get e getbyid

--- mycontacts/src/index.js
+++ mycontacts/src/index.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import routes from "./routes.js"
+import express from 'express'
+import routes from './routes.js'
 
-const app = express();
+const app = express()
 
-app.use(routes);
+app.use(routes)
 
 app.listen(3000, () => {
-  console.log('Server started at http://localhost:3000');
-});
+  console.log('Server started at http://localhost:3000')
+})
--- mycontacts/src/routes.js
+++ mycontacts/src/routes.js
@@ -1,11 +1,11 @@
-import { Router } from 'express';
-import ContactController from './app/controller/ContactController.js';
+import { Router } from 'express'
+import ContactController from './app/controller/ContactController.js'
 
+const router = Router()
 
-const router = Router();
-
-router.get('/contacts', ContactController.index);
+router.get('/contacts', ContactController.index)
+router.get('/contacts/:id', ContactController.show)
+router.delete('/contacts/:id', ContactController.delete)
 
 
 export default router
-
